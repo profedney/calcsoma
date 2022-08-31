a6abcba4f86fcea2a7342f355348ff5e4ccc789a
profedney <111137546+profedney@users.noreply.github.com>
2022-08-31 12:13:45 -0300

    calculadora soma funcionando

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function calcSum() {
-    let num1 = document.getElementsByName("num1")[0].value;
-    let sum = Number(num1);
-    document.getElementsByName("soma")[0].value = sum;
+    let num1 = document.getElementsByName("n1")[0].value;
+    let number2 = document.getElementsByName("n2")[0].value;
+    let sum = Number(num1) + Number(number2);
+    document.getElementsByName("somado")[0].value = sum;
 }
