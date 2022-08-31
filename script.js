5241d014d8ded9f74198a86a61a0948e9a4095a2
profedney <111137546+profedney@users.noreply.github.com>
2022-08-31 11:27:58 -0300

    adicionado javascrip

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("Calculadora soma dos alunos Marta Teresinha");
+function calcSum() {
+    let num1 = document.getElementsByName("num1")[0].value;
+    let sum = Number(num1);
+    document.getElementsByName("soma")[0].value = sum;
+}
